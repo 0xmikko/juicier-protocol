360194043a935e671a1bdf0446a14485e7203394
Mikhail Lazarev <mikael.lazarev@gmail.com>
2020-10-07 20:24:01 +0300

    Typescript env is set up

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -1,33 +1,48 @@
 pragma solidity ^0.5.0;
 
-import "core/AddressStorage.sol"
+import "./core/AddressStorage.sol";
 
 contract Pool {
-  address public owner = msg.sender;
+    address public owner = msg.sender;
 
-  AddressStorage reserves;
+    AddressStorage reserves;
 
-   /**
-    * @dev functions affected by this modifier can only be invoked if the provided _amount input parameter
-    * is not zero.
-    * @param _amount the amount provided
-    **/
+    /**
+     * @dev functions affected by this modifier can only be invoked if the provided _amount input parameter
+     * is not zero.
+     * @param _amount the amount provided
+     **/
     modifier onlyAmountGreaterThanZero(uint256 _amount) {
         requireAmountGreaterThanZeroInternal(_amount);
         _;
     }
 
+    modifier onlyOwner {
+        require(
+            msg.sender == owner,
+            "This function could be called by owner only"
+        );
+        _;
+    }
 
-  modifier onlyOwner {
-    require(msg.sender == owner, "This function could be called by owner only")
-  }
-
-    function deposit(address _reserve, uint256 _amount, uint16 _referralCode)
+    function deposit(
+        address _reserve,
+        uint256 _amount,
+        uint16 _referralCode
+    )
         external
         payable
         // onlyUnfreezedReserve(_reserve)
         onlyAmountGreaterThanZero(_amount)
-    {
-    }
+    {}
 
+    /**
+     * @notice internal function to save on code size for the onlyAmountGreaterThanZero modifier
+     **/
+    function requireAmountGreaterThanZeroInternal(uint256 _amount)
+        internal
+        pure
+    {
+        require(_amount > 0, "Amount must be greater than 0");
+    }
 }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
-const Migrations = artifacts.require("Migrations");
-
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Migrations = artifacts.require('Migrations');
+module.exports = function (deployer) {
+    deployer.deploy(Migrations);
 };
+//# sourceMappingURL=1_initial_migration.js.map
