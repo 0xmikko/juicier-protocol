0831fff0ec08bcf9600d11d290e84702f3d93f84
Mikhail Lazarev <mikael.lazarev@gmail.com>
2020-10-20 23:47:29 +0300

    Contract js fix

--- landing/components/Web3/getContract.tsx
+++ landing/components/Web3/getContract.tsx
@@ -1,14 +1,17 @@
-const getContractInstance = async (web3, contractDefinition) => {
-    // get network ID and the deployed address
-    const networkId = await web3.eth.net.getId()
-    const deployedAddress = contractDefinition.networks[networkId].address
+import Web3 from "web3";
 
-    // create the instance
-    const instance = new web3.eth.Contract(
-        contractDefinition.abi,
-        deployedAddress
-    )
-    return instance
-}
+export const getContractInstance = async (
+  web3: Web3,
+  contractDefinition: any
+) => {
+  // get network ID and the deployed address
+  const networkId = await web3.eth.net.getId();
+  const deployedAddress = contractDefinition.networks[networkId].address;
 
-export default getContractInstance
+  // create the instance
+  const instance = new web3.eth.Contract(
+    contractDefinition.abi,
+    deployedAddress
+  );
+  return instance;
+};
